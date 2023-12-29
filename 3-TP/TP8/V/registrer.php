faac6d4c6380c1cd2e8c62fcd7feb402d4e6d01f
LMNoire <149476454+LMNoire@users.noreply.github.com>
2023-12-29 12:38:19 +0100

    Update TP8

--- 3-TP/TP8/V/registrer.php
+++ 3-TP/TP8/V/registrer.php
@@ -4,8 +4,8 @@
 <head>
 <meta charset="utf-8" />
 <link rel="stylesheet" href="button.css">
-<title>Shuffle</title>
-<link rel="stylesheet" href="style.css">
+<title>Inscription</title>
+<link rel="stylesheet" href="register.css">
 
 </head>
 
@@ -13,34 +13,35 @@
 
     
 <form method="post" action="../C/createUser.action.php" name="form" id="formContact">
-  <div class="title">Créer</div>
-  <div class="subtitle">Créeons votre Compte!</div>
+    <!-- <div class="title">Créer</div>
+    <div class="subtitle">Créeons votre Compte!</div> -->
+    <h1>Inscription</h1>
 
-  <div class="input-container ic1">
-    <input name="name" class="input" type="text" placeholder=" "/>
-    <label for="name" class="placeholder">Nom</label>
-  </div>
+    <div class="inscription">
+    
+        <input type="text" class="profil" name="name" placeholder="Nom" required>
+      
+        <input type="text" class="profil" name="firstname" placeholder="Prenom" required>
 
-  <div class="input-container ic2">
-    <input name="firstname" class="input" type="text" placeholder=" "/>
-    <label for="firstname" class="placeholder">Prenom</label>
-  </div>
+        <input type="email" class="profil" name="mail"  placeholder="Mail" required>
+      
+        <input type="tel" class="profil" name="tel" placeholder="Téléphone" required>
 
-  <div class="input-container ic2">
-    <input name="mail" class="input" type="text" placeholder=" "/>
-    <label for="mail" class="placeholder">mail</label>
-  </div>
+        <input type="password" class="profil" name="password" placeholder="password" required>
+        
+        <input type="password" class="profil" placeholder="Confirmer Password" required>
+    </div>  
+<p class= sengager>s'engager ?</p>
 
-  <div class="input-container ic2">
-    <input name="tel" class="input" type="text" placeholder=" "/>
-    <label for="tel" class="placeholder">tel</label>
-  </div>
 
-  <div class="input-container ic2">
-    <input name="password" class="input" type="text" placeholder=" "/>
-    <label for="password" class="placeholder">Mot De Passe (6 caractères minimum)</label>
-  </div>
+  
+<input type="image" src="../V/assets/sceau.jpeg"  class="sceau" value =" "/>   
 
-  <button type="text" class="submit">Envoyer</button>
-</div>
+
 </form>
+
+
+
+
+</body>
+</html>
