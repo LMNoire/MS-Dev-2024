8a4b42c91ee6bfc13a9e757afc3b1a5f2badfdbb
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-01-09 12:41:35 +0100

    Intégration API autocomplétion adresse

--- 3-TP/TP9/V/register.php
+++ 3-TP/TP9/V/register.php
@@ -23,9 +23,16 @@
         <input type="text" class="profil" name="firstname" placeholder="Prénom" id="firstname" required>
         <input type="email" class="profil" name="mail"  placeholder="Mail" id="mail" required>
         <input type="tel" class="profil" name="tel" placeholder="Téléphone" id="tel" required>
-        <input type="text" class="profil" name="adresse" placeholder="Adresse" id="adresse">
         <input type="password" class="profil" name="password" placeholder="Password" id="password" required>
-        <input type="password" class="profil" placeholder="Confirmer Password" required>
+        <input type="password" class="profil" placeholder="Confirmer Password" id="confirmpass" required>
+            <!--Adresse-->
+            <input type="text" class="profil" id="inputRue" name="inputRue" placeholder="Adresse">   
+            <!--Suggestions API adresse-->
+            <div class="suggestions-container">
+                <ul class="adress-suggestions"></ul>
+            </div>         
+            <input type="text" class="profil" id="inputCodePostal" minlength="5" maxlength="5" placeholder="Code Postal">
+            <input type="text" class="profil" id="inputVille" name="inputVille" placeholder="Ville">
     </div>  
 <p class= sengager>S'engager ?</p>
 <!--Bouton validation-->
@@ -38,36 +45,74 @@
 </body>
 </html>
 
+<!--Script pour récupérer infos du formulaire-->
 <script>
-$(document).ready(function(){
- 
- $("#submit").click(function(e){
-     e.preventDefault();
+$(document).ready(function() {
+    
+    /*Autocompletion adresse API gouv*/
+    $("#inputRue").keyup(function(event) {
+        event.preventDefault();
+        
+        let rue = $("#inputRue").val();
+        /*Requete*/
+        $.get('https://api-adresse.data.gouv.fr/search/', {
+            q: rue,
+            limit: 15,
+            autocomplete: 1
+        }, function(data, status, xhr) {
+            let suggestions = "";
+            $.each(data.features, function(i, obj) {
+                suggestions += '<li class="address-item" data-name="' + obj.properties.name + '" data-postcode="' + obj.properties.postcode + '" data-city="' + obj.properties.city + '">' + obj.properties.label + '</li>';
+            });
+            $('.adress-suggestions').html(suggestions);
+        }, 'json').fail(function() {
+            console.log("Error fetching address suggestions.");
+        });
+    });
 
-         $.ajax({
-         url:"../C/createUser.action.php",
-         type:'POST',
-         dataType: 'json', //text
-         data: {
-             name : $("#name").val(),  // Nous récupérons la valeur de nos input que l'on fait passer à connexion.php
-             firstname : $("#firstname").val(),
-             mail : $("#mail").val(),
-             tel : $("#tel").val(),
-             password : $("#password").val()
-             
-         },
-         success:function(response) {		
-          //var json = $.parseJSON(response);
-             console.log(response);
-             alert(response.name + response.firstname);
-         },
-         error:function(response) {
-             console.log('error');
-             alert("error");
+    /*Afficher suggestions*/
+    $('.adress-suggestions').on("click", ".address-item", function() {
+        $("#inputRue").val($(this).attr("data-name"));
+        $("#inputCodePostal").val($(this).attr("data-postcode"));
+        $("#inputVille").val($(this).attr("data-city"));
 
-         }
-         
-     });
+        /*Vider champ suggestion*/
+        $('.adress-suggestions').empty();
+    });
+
+    /*Envoi du formulaire au controleur*/
+    $("#submit").click(function(e){
+        e.preventDefault();
+            /*Call Ajax*/
+            $.ajax({
+            /*Chemin controleur*/
+            url:"../C/createUser.action.php",
+            type:'POST',
+            dataType: 'json',
+            data: {
+                name : $("#name").val(), 
+                firstname : $("#firstname").val(),
+                mail : $("#mail").val(),
+                tel : $("#tel").val(),
+                password : $("#password").val(),
+                adresse: $("#adresse").val()
+            },
+            /*Success*/
+            success:function(response) {		
+            //var json = $.parseJSON(response);
+                console.log(response);
+                /*Afficher résultat dans pop-up*/
+                alert(response.name + response.firstname);
+                /*Clean output fields*/
+                $("form")[0].reset();
+            },
+            /*False*/
+            error:function(response) {
+                console.log('error');
+                alert("error");
+            }
+        });
+    });
  });
-});
+
 </script>
