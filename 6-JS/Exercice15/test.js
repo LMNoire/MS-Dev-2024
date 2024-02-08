d27308aa3691860daf6403597c787b73df59803b
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-02-08 14:15:32 +0100

    Update 6-JS exercice 15

--- 6-JS/Exercice15/test.js
+++ 6-JS/Exercice15/test.js
@@ -43,50 +43,48 @@ $(document).ready(function() {
         $('.suggestions-container').hide();
         });
     /*ZipCode END*/
-    })
-
-/*Street START*/
-$("#street").keyup(function(e) {
-    e.preventDefault();
-
-    let city = $("#city").val();
-
-    /*Query for street*/
-    $.get('https://api-adresse.data.gouv.fr/search/?q=' + city, {
-        q: $(this).val(), 
-        limit: 10,
-        autocomplete: 1,
-        dataType: 'json' 
-    }, function(data, status, xhr) {
-        if (data.features && data.features.length > 0) {
-            let suggestions = "";
-            $.each(data.features, function(i, obj) {
-                suggestions += '<li class="street-suggestions">' + obj.properties.street + '</li>';
-            });
-            /* Append suggestions to the container */
-            $('.street-suggestions').html(suggestions);
-            /* Display suggestion container */
-            $('.suggestions-container2').show();
-        } else {
-            /* Hide suggestion container */
-            $('.suggestions-container2').hide();
-        }
-        /*If error*/
-    }, 'json').fail(function() {
-        console.log("Error fetching address suggestions.");
     });
-});
 
-/* Event delegation for street suggestions */
-$('.street-suggestions').on("click", ".street-suggestions", function() {
-    $("#street").val($(this).text());
-    /*Clear suggestions*/
-    $('.suggestions-container2').hide();
-});
-/*Street END*/
-
+    /*Street START*/
+    $("#street").keyup(function(e) {
+        e.preventDefault();
+        let city = $("#city").val();
+        /*Query for street*/
+        $.get('https://api-adresse.data.gouv.fr/search/?q=' + city, {
+            q: $(this).val(), 
+            limit: 10,
+            autocomplete: 1,
+            dataType: 'json' 
+        }, function(data, status, xhr) {
+            if (data.features && data.features.length > 0) {
+                let suggestions = "";
+                $.each(data.features, function(i, obj) {
+                    suggestions += '<li class="street-suggestions">' + obj.properties.street + '</li>';
+                });
+            /*Display suggestions to the container*/
+            $('.street-suggestions').html(suggestions);
+            /*Display suggestion container*/
+            $('.suggestions-container2').show();
+                } else {
+            /*Hide suggestion container*/
+            $('.suggestions-container2').hide();
+                }
+            /*If error*/
+            }, 'json').fail(function() {
+                console.log("Error fetching address suggestions.");
+            });
+        });
+        /*Display suggestions*/
+        $('.street-suggestions').on("click", ".street-suggestions", function() {
+        $("#street").val($(this).text());
+        /*Clear suggestions*/
+        $('.suggestions-container2').hide();
+    /*Street END*/    
+    });
+    
 /*End of document*/        
 })
+
 
 
         
