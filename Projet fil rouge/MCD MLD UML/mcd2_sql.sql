98545eb75bf74aedbeb20bfd13f9f73dd3810839
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-02-22 09:52:59 +0100

    Update MCD/MLD

--- Projet fil rouge/MCD MLD UML/mcd2_sql.sql
+++ Projet fil rouge/MCD MLD UML/mcd2_sql.sql
@@ -16,9 +16,9 @@ CREATE TABLE User(
         city          Varchar (50) NOT NULL ,
         street        Varchar (100) NOT NULL ,
         phone         Varchar (25) NOT NULL ,
-        is_confirmed  Bool NOT NULL ,
-        role          Int NOT NULL ,
-        hash_password Varchar (250) NOT NULL
+        role          Varchar (50) NOT NULL ,
+        hash_password Varchar (250) NOT NULL ,
+        cpt_ope       Int  Auto_increment  NOT NULL
 	,CONSTRAINT User_PK PRIMARY KEY (id_user)
 )ENGINE=InnoDB;
 
@@ -28,25 +28,24 @@ CREATE TABLE User(
 #------------------------------------------------------------
 
 CREATE TABLE Operation(
-        id_ope            Int  Auto_increment  NOT NULL ,
-        type              Int NOT NULL ,
-        name              Varchar (100) NOT NULL ,
-        price             Int NOT NULL ,
-        description       Varchar (250) NOT NULL ,
-        status            Int NOT NULL ,
-        created_at        Datetime NOT NULL ,
-        rdv_at            Datetime NOT NULL ,
-        zipcode_ope       Varchar (10) NOT NULL ,
-        city_ope          Varchar (50) NOT NULL ,
-        street_ope        Varchar (100) NOT NULL ,
-        customer          Varchar (100) NOT NULL COMMENT "Récupérer l'id_user du client qui achete la prestation"  ,
-        salarie           Varchar (100) NOT NULL COMMENT "Récupérer l'id_user du salarie qui s'occupe de la prestation"  ,
-        id_user           Int NOT NULL ,
-        id_user_Accomplir Int NOT NULL
+        id_ope      Int  Auto_increment  NOT NULL ,
+        type        Int NOT NULL ,
+        name        Varchar (100) NOT NULL ,
+        price       Int NOT NULL ,
+        description Varchar (250) NOT NULL ,
+        status      Int NOT NULL ,
+        created_at  Datetime NOT NULL ,
+        rdv_at      Datetime NOT NULL ,
+        zipcode_ope Varchar (10) NOT NULL ,
+        city_ope    Varchar (50) NOT NULL ,
+        street_ope  Varchar (100) NOT NULL ,
+        customer    Varchar (100) NOT NULL COMMENT "Récupérer l'id_user du client qui achete la prestation"  ,
+        salarie     Varchar (100) NOT NULL COMMENT "Récupérer l'id_user du salarie qui s'occupe de la prestation"  ,
+        finished_at Datetime NOT NULL ,
+        id_user     Int NOT NULL
 	,CONSTRAINT Operation_PK PRIMARY KEY (id_ope)
 
 	,CONSTRAINT Operation_User_FK FOREIGN KEY (id_user) REFERENCES User(id_user)
-	,CONSTRAINT Operation_User0_FK FOREIGN KEY (id_user_Accomplir) REFERENCES User(id_user)
 )ENGINE=InnoDB;
 
 
@@ -55,11 +54,13 @@ CREATE TABLE Operation(
 #------------------------------------------------------------
 
 CREATE TABLE Documents(
-        id_doc   Int  Auto_increment  NOT NULL ,
-        type     Int NOT NULL ,
-        customer Varchar (50) NOT NULL ,
-        url      Varchar (250) NOT NULL ,
-        id_ope   Int NOT NULL
+        id_doc    Int  Auto_increment  NOT NULL ,
+        type      Int NOT NULL ,
+        customer  Varchar (50) NOT NULL ,
+        url       Varchar (250) NOT NULL ,
+        salarie   Varchar (50) NOT NULL ,
+        operation Varchar (50) NOT NULL ,
+        id_ope    Int NOT NULL
 	,CONSTRAINT Documents_PK PRIMARY KEY (id_doc)
 
 	,CONSTRAINT Documents_Operation_FK FOREIGN KEY (id_ope) REFERENCES Operation(id_ope)
