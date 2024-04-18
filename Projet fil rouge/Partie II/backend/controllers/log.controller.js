54450cf173d19650414457b9e5736340d929e88d
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-04-18 15:13:31 +0200

    Projet fil rouge update partie I & II

--- Projet fil rouge/Partie II/backend/controllers/log.controller.js
+++ Projet fil rouge/Partie II/backend/controllers/log.controller.js
@@ -1,7 +1,7 @@
 //Import Log Schema as LogModel
 const LogModel = require('../models/log.model');
 
-//Set logs method
+//Method to post logs
 module.exports.setLogs = async (req, res) => {
     const log = await LogModel.create({
         loggerName: req.body.loggerName,
@@ -13,22 +13,10 @@ module.exports.setLogs = async (req, res) => {
     res.status(200).json(log);
 };
 
-//Get all logs method
+//Method to get all logs
 module.exports.getLogs = async (req, res) => {
-    const logs = await LogModel.find();
-    res.status(200).json(logs)
+    const logs = await LogModel.find({ loggerName: "User" });
+    res.status(200).json(logs);
 };
 
-//Get log by id
-module.exports.getLogByUser = async (req, res) => {
-    // try {
-        const logsUsers = await db.collection('cleanthis').find({ user: req.params.user });
-        // if (!logs) {
-        //     return res.status(400).json({ message: "Ce log n'existe pas" });
-        // }
-        res.status(200).json(logsUsers);
-    // } catch (error) {
-    //     res.status(500).json({ message: "Une erreur s'est produite lors de la récupération du log" });
-    // }
-};
 
--- Projet fil rouge/Partie II/backend/routes/log.routes.js
+++ Projet fil rouge/Partie II/backend/routes/log.routes.js
@@ -1,23 +1,12 @@
 //Import express lib
 const express = require("express");
 //Import methods
-const { setLogs, getLogs, getLogByUser } = require("../controllers/log.controller");
+const { setLogs, getLogs } = require("../controllers/log.controller");
 const router = express.Router();
 
-//Get logs
-router.get("/", getLogs);
-//Get logs by user
-router.get("/:user", getLogByUser);
 //Post logs
 router.post("/", setLogs);
-
-//Put
-router.put("/:id", (req, res) => {
-    res.json({ messageId: req.params.id });
-});
-//Delete
-router.delete("/:id", (req, res) => {
-    res.json({ message: "Suppression effective : " + req.params.id });
-});
+//Get logs
+router.get("/", getLogs);
 
 module.exports = router
